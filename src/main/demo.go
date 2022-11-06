fcba1803ee796b106c39d65e30023a78b617a3c5
Alessandro Cuppari <me@alessandrojcm.dev>
2022-11-06 17:29:21 +0000

    feat: added flag to show the texture in a window

--- src/main/demo.go
+++ src/main/demo.go
@@ -16,7 +16,7 @@ func main() {
 	runtime.LockOSThread()
 	var rows, columns, cellSize, wallThickness int
 	var bias, algorithm string
-	var export, colorTiles bool
+	var export, colorTiles, show bool
 
 	flag.IntVar(&rows, "rows", 4, "number or rows for the maze")
 	flag.IntVar(&columns, "columns", 4, "number of columns for the maze")
@@ -28,6 +28,7 @@ func main() {
 	flag.IntVar(&cellSize, "cellsize", 10, "sets the size of the cells")
 	flag.IntVar(&wallThickness, "thickness", 1, "sets the thickness of the walls for the exported images")
 	flag.BoolVar(&colorTiles, "tiles", false, "colour the cells")
+	flag.BoolVar(&show, "show", false, "show result in window")
 	flag.Parse()
 
 	grid, err := mfp.NewGrid(rows, columns)
@@ -42,12 +43,28 @@ func main() {
 	}
 	fmt.Printf("Printing %s %vx%v maze with %s bias\n", algorithm, rows, columns, bias)
 	fmt.Println(grid)
-	if export {
+	if export || show {
 		target := grid.ToTexture(cellSize, wallThickness, colorTiles)
-		img := rl.LoadImageFromTexture(target.Texture)
-		rl.ImageFlipVertical(*&img)
+		name := fmt.Sprintf("%s-%vrowX%vcol-%s-%v.png", algorithm, rows, columns, bias, time.Now().UnixNano())
+		if export {
+			img := rl.LoadImageFromTexture(target.Texture)
+			rl.ImageFlipVertical(*&img)
+			defer rl.UnloadImage(img)
+			rl.ExportImage(*img, name)
+		}
+
+		if show {
+			rl.ClearWindowState(rl.FlagWindowHidden)
+			rl.SetWindowTitle(name)
+			defer rl.UnloadTexture(target.Texture)
+			rl.SetTargetFPS(60)
+			for !rl.WindowShouldClose() {
+				rl.BeginDrawing()
+				rl.DrawTexture(target.Texture, 0, 0, rl.White)
+				rl.EndDrawing()
+			}
+		}
 		defer rl.UnloadRenderTexture(*target)
-		defer rl.UnloadImage(img)
-		rl.ExportImage(*img, fmt.Sprintf("%s-%vrowX%vcol-%s-%v.png", algorithm, rows, columns, bias, time.Now().UnixNano()))
+		defer rl.CloseWindow()
 	}
 }
